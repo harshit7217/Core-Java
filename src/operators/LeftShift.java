27873f0c6e65e898ff87df28faa8547e04e77fa8
Prasang41 <harshitchaudhary568@gmail.com>
2026-07-25 22:23:59 +0530

    Left Shifting a byte value

---
+++ src/operators/LeftShift.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class LeftShift {
+    public static void main(String[] args) {
+        byte a = 64, b;
+        int i;
+
+        i = a << 2;
+        b = (byte) ( a << 2);
+
+        System.out.println("Original Data: "+ a);
+        System.out.println("i & b: "+ i + " "+ b);
+    }
+}
