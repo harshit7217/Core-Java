06cfccdce06526334f296601e286317bd1eb235f
Prasang41 <harshitchaudhary568@gmail.com>
2026-07-27 19:31:51 +0530

    The Right Shift Operator

---
+++ src/operators/HexaByte.java
@@ -0,0 +1,14 @@
+package operators;
+
+//Masking sign extension
+
+public class HexaByte {
+    public static void main(String[] args) {
+        char[] hex = {
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+        };
+
+        byte b = (byte) 0xf1;
+        System.out.println("b = 0x" + hex[(b >> 0x0f) & 0x0f] + hex[b & 0x0f]);
+    }
+}
